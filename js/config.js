6ae06943dc16602fc77c4c7c6160ab6af6ad0b03
Sushant Patil <patsusha@amazon.com>
2021-04-27 19:54:01 -0400

    Copying project to guthub

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_aXUwkRmhG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5126qp1em1gpatk1q152i8nf53', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://vj4fmlljh4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
